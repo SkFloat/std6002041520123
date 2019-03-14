960b3518854900fc06bc65faa2ce709bf7bae682
ศรัณย์ คัดชานันท์ <s6002041520123@Macmini-213-20.local>
2019-03-14 16:39:11 +0700

    create App component

---
+++ components/App.js
@@ -0,0 +1,17 @@
+//import
+import React from 'react';
+import { View, Text } from 'react-native';
+//write
+class App extends React.Component {
+Renderi() {
+    return (
+        //com
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
+
+}
+//Export
+export default App;
