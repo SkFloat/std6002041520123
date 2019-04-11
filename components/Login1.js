d40ab0fde065d7dfa12ea26ba00e61b7b51f7b04
ศรัณย์ คัดชานันท์ <s6002041520123@Macmini-213-20.local>
2019-04-11 16:58:52 +0700

    edit login

---
+++ components/Login1.js
@@ -0,0 +1,74 @@
+// import library
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+
+// write component
+class Login extends Component {
+    static navigationOptions = {
+        title: 'Login'
+    }
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+        // this.onChangeEmail = this.onChangeEmail.bind(this)
+    }
+    async componentDidMount() {
+        const {navigate} = this.props.navigation;   
+        const token = await AsyncStorage.getItem('@storage_Token')
+        if (token) {
+            return navigate('Profile')
+        }
+    }
+    onChangeEmail(e) {
+        console.log('onChangeEmail', e)
+        this.setState({ email: e}) 
+    }
+    onChangePassword(e) {
+        this.setState({ password: e})
+    }
+    async onPress() {
+        // console.log(this.state)
+        const url = 'http://128.199.240.120:9999/api/auth/login'
+        await axios.post(url, this.state)
+            .then(async response => {
+                // console.log('token ', response.data.data.token)
+                await AsyncStorage.setItem('@storage_Token', response.data.data.token)
+            })
+
+        
+        // console.log('token', token)
+    }
+    render() {
+        // const {navigate} = this.props.navigation;
+        // return navigate('Profile')
+        return (
+            <View>
+                <TextInput
+                    style={{ height: 40, fontWeight: 'bold', fontSize: 20 }}
+                    placeholder="Email"
+                    value={this.state.email}
+                    onChangeText={this.onChangeEmail.bind(this)}
+                />
+                <TextInput
+                    secureTextEntry
+                    style={{ height: 40, fontWeight: 'bold', fontSize: 20 }}
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChangeText={this.onChangePassword.bind(this)}
+                />
+                <Button  
+                    title="Login"
+                    onPress={this.onPress.bind(this)}
+                />
+            </View>
+        );
+    }
+}
+
+// export
+export default Login;
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 /**
  * @format
  */
+
 import {AppRegistry} from 'react-native';
 //import App from './App';
 import App from './components/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+
+/* teacher
+import {AppRegistry} from 'react-native';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
+// import App from './App';
+import {name as appName} from './app.json';
+// import App from './components/App';
+import AboutScreen from './components/About';
+import LoginScreen from './components/Login';
+
+///
+
+const MainNavigator = createStackNavigator({
+    Login: {screen: LoginScreen},
+    Profile: {screen: AboutScreen},
+});
+const App = createAppContainer(MainNavigator);
+
+AppRegistry.registerComponent(appName, () => App);*/
